0dcca25d0cae76302b741ba653dea9458a5fb27f
sovy <isovyzeze@gmail.com>
2012-04-05 22:26:09 +0000

    modificari exprimare

--- Fluxul de executie si apeluri nested la functii.php
+++ Fluxul de executie si apeluri nested la functii.php
@@ -1,10 +1,10 @@
  Fie codul:
 <?php
 function foo($a) {
-return $a +1;
+	return $a +1;
 }
 function bar($b) {
-return $b *42;
+	return $b *42;
 }
 echo foo(bar(foo(foo (38))+2));
 
@@ -13,14 +13,14 @@ de adunare si de inmultire care sunt executate.
 
  Prima oara este apelata functia foo avand valoarea parametrului 38, 
 valoarea returnata in urma acestui apel la functia foo este 39(38+1),
-dupa care functia foo este apelata din nou dar de data aceasta valoarea 
-parametrului este retvalue-ul functiei foo(adica 39) in urma apelului 
+dupa care functia foo este apelata din nou, dar de data aceasta valoarea 
+parametrului este retvalul functiei foo(adica 39) in urma apelului 
 de mai devreme a functiei foo, valoarea returnata de acest apel este 
 40(39+1), aceasta valoarea se aduna cu 2 rezultand valoarea 42, se 
 apeleaza functia bar, iar valoarea parametrului pasat acestei functii 
 are valoarea rezultata in urma adunarii de mai devreme
 adica 42, dupa care se apeleaza din nou functia foo avand valoarea
-parametrului, retval-ul functiei bar(42*42), adica 1764. 
+parametrului, retvalul functiei bar(42*42), adica 1764. 
  Valoarea finala returnata este de 1765(1764+1). Aceasta valoare ii 
 este pasata lui echo pentru a fi afisata.
  
