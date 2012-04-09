853082a10bb1d3d7a6689f32881581bcfeb41d4e
sovy <isovyzeze@gmail.com>
2012-04-09 16:33:10 +0000

    Insert at right position

---
+++ Insert at right position.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * insert at right position a third number($c) knowing the other two's 
+ * order($a <= $b)
+ *
+ * int insert_at_right_position(int $a, int $b, int $c)
+ *
+ *
+ */
+
+function insert_at_right_position($a, $b, $c) {
+    if($c <= $a) {
+        $m = array($c, $a, $b); 
+    }
+    elseif($c >= $b) {
+        $m = array($a, $b, $c);
+    }
+    else {
+        $m = array($a, $c, $b);
+    }
+    return $m;
+}
+
+
+$a = (int) $_GET['a'];
+$b = (int) $_GET['b'];
+$c = (int) $_GET['c'];
+$message = array();
+if($a <= $b) {
+    list($a, $b, $c) = insert_at_right_position($a, $b, $c);
+}
+else {
+    list($a, $b) = aray($b, $a);
+    list($a, $b, $c) = insert_at_right_position($a, $b, $c);
+}
+
+echo $a, ' <= ', $b, ' <= ', $c;
