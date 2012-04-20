95a14b7b340f409eabbe1974274575e16dfee5d8
sovy <isovyzeze@gmail.com>
2012-04-20 18:23:43 +0000

    Adaugare verificarea  parametrilor

--- Creaza o functie variadica I.php
+++ Creaza o functie variadica I.php
@@ -4,16 +4,21 @@
  * int foo(int $par[, int $...]);
  *
  * variadic function -returns the product of its arguments; 
- * 
+ * in case of no arguments passed or at least one argument isn't 
+ * integer , the retval will be 0;
  */
 
 function foo() {
     for($i = 0, $arg_prod = 1; $i < func_num_args(); $i++) {
+        if(!is_int(func_get_arg($i))){
+            $arg_prod = 0;
+            break;
+        }
         $arg_prod *= func_get_arg($i);
     }
     return $arg_prod;
 }
 
 
-echo foo(2, 3, 5);
+echo foo(2, 1.2);
 
