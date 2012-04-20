0b936968fe8751db26ba23773b232860d9a0373e
sovy <isovyzeze@gmail.com>
2012-04-20 18:54:33 +0000

    Functie variadica partea a II -a

---
+++ Creaza o functie variadica II.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * int array_product(mix $par[, mix $...]);
+ *
+ * This function returns the product of an array elements; 
+ * in case if the value passed as parameter is array() or at least one
+ * array element isn't numeric , the retval will be 0;
+ *
+ */
+
+function array_products($par) { 
+    $product = 0;
+    if($par !== array()) {
+        $product = 1;
+        foreach($par as $key => $value) {
+            if(!is_numeric($value)) {
+                $product = 0;
+                break;
+            }
+        $product *= $value;
+        }
+    }
+        return (int) $product;
+        
+}
+
+$par = array();
+
+echo array_products($par);
